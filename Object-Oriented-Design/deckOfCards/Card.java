5c20de28362e25ac1577a831776195497b92c521
hughmancoder <hugh.signoriello@gmail.com>
2023-11-29 23:47:42 +1030

    add card OO design

---
+++ Object-Oriented-Design/deckOfCards/Card.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Card {
+    private final Suit suit;
+    private final Rank rank;
+
+    public Card(Suit suit, Rank rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+    
+}
